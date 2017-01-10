c79f33fc7a81bb82f700717bb6c9cc826bb2250b
Zachary Keeton <zachary.keeton@gmail.com>
2017-01-09 21:42:13 -0600

    added new unit test

--- test/test.js
+++ test/test.js
@@ -11,4 +11,8 @@ describe('first tests',function(){
 	it("should return 10 with 5 and 5",function(){
 		add(5,5).should.equal(10);
 	});
+
+	it("should return 0 with -100 and 100", function(){
+		add(-100,100).should.equal(0);
+	});
 });
